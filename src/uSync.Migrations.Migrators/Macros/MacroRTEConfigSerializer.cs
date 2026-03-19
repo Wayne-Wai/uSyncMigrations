1ee94e04c7004bd779a649d2a3906e4fa22c3cd7
Kevin Jump <kevin@thejumps.co.uk>
2026-03-19 15:41:15 +0000

    Add Macro blocks to RTE allowed blocks so we can migrate them.

---
+++ src/uSync.Migrations.Migrators/Macros/MacroRTEConfigSerializer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.Services;
+
+using uSync.Core.DataTypes;
+using uSync.Core.Extensions;
+using uSync.Migrations.Core.Extensions;
+using uSync.Migrations.Core.Migrators;
+
+namespace uSync.Migrations.Migrators.Macros;
+
+
+/// <summary>
+///  adds any generated Macro Content Types to the list of allowed blocks inside an RTE.
+/// </summary>
+internal class MacroRTEConfigSerializer : SyncConfigurationMigratorBase, IConfigurationSerializer
+{
+    private readonly IContentTypeService _contentTypeService;
+    private readonly IContentTypeContainerService _contentTypeContainerService;
+
+    public MacroRTEConfigSerializer(
+        IContentTypeService contentTypeService,
+        IContentTypeContainerService contentTypeContainerService)
+    {
+        _contentTypeService = contentTypeService;
+        _contentTypeContainerService = contentTypeContainerService;
+    }
+
+    public string Name => nameof(MacroRTEConfigSerializer);
+
+    public string[] Editors => [Constants.PropertyEditors.Aliases.RichText];
+
+    // keep it the same. 
+    public override string? TargetEditor => null;
+
+    public override async Task<IDictionary<string, object>> GetMigratedConfigurationAsync(string name, IDictionary<string, object> configuration)
+    {
+        var richTextBlocks = new List<RichTextConfiguration.RichTextBlockConfiguration>();
+
+        if (configuration.TryGetValue("blocks", out var blocksConfig))
+        {
+            var blocksJson = blocksConfig.SerializeJsonString();
+            richTextBlocks = blocksJson.DeserializeJson<
+                List<RichTextConfiguration.RichTextBlockConfiguration>>() ?? [];
+        }
+
+        if (richTextBlocks.Count == 0)
+            return configuration;
+
+        var macroContentTypes = await GetMacroContentTypes();
+        foreach (var macroContentType in macroContentTypes)
+        {
+            if (richTextBlocks.Any(x => x.ContentElementTypeKey == macroContentType.Key) is true)
+                continue;
+
+            richTextBlocks.Add(new RichTextConfiguration.RichTextBlockConfiguration
+            {
+                ContentElementTypeKey = macroContentType.Key,
+            });
+        }
+
+        var updatedConfig = configuration.ToDictionary();
+        updatedConfig["blocks"] = richTextBlocks;
+
+        return updatedConfig;
+    }
+
+    private async Task<IEnumerable<IContentType>> GetMacroContentTypes()
+    {
+        List<EntityContainer> macroContainer = [.. await _contentTypeContainerService.GetAsync("Macros", 1)];
+        if (macroContainer.Count == 0) return [];
+        return _contentTypeService.GetChildren(macroContainer[0].Id);
+    }
+
+}
